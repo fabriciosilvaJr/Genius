661210df31156408d0c53fecb199e6c95a25c6fc
fabriciosilvaJr <fabriciosilva187@gmail.com>
2022-02-26 20:51:02 -0300

    order, time

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random()*4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i  in order){
+        let elementColor = createElement(order[i]);
+        lightColor(elementColor, Number[i]+ 1)
+    }
+}
+
+let lightColor = (element, number) =>{
+    number = number + 500;
+    setTimeout(() => {
+        element.classList.add('selected')
+    },number - 250);
+
+    setTimeout(() =>{
+        element.classList.remove('selected')
+    })
+
+}
