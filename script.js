fbf1e23492ef2e06bf063c066c60c4d2457b20bf
fabriciosilvaJr <fabriciosilva187@gmail.com>
2022-02-27 11:08:52 -0300

    compara ordem dos clicks

--- script.js
+++ script.js
@@ -12,17 +12,18 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+//cria ordem aleatória de cores
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random()*4);
     order[order.length] = colorOrder;
     clickedOrder = [];
 
     for(let i  in order){
-        let elementColor = createElement(order[i]);
+        let elementColor = createColorElement(order[i]);
         lightColor(elementColor, Number[i]+ 1)
     }
 }
-
+//acende a próxima cor
 let lightColor = (element, number) =>{
     number = number + 500;
     setTimeout(() => {
@@ -32,5 +33,34 @@ let lightColor = (element, number) =>{
     setTimeout(() =>{
         element.classList.remove('selected')
     })
+
+}
+//checa se os botões clicados são os mesmos de ordem gerada no jogo
+let checkOrder = () =>{
+    for(let i in clickedOrder){
+        if(clickedOrder[i] != order[i]){
+            lose();
+            break;
+
+        }
+           
+    }
+    if(clickedOrder.length == order.length){
+        alert(`Pontuação: ${score}\nVocê acertou! iniciando próximo nível`);
+        nextLevel();
+    }
+
+}
+
+//função para o clique do usuário
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() =>{
+        createColorElement(color).classList.remove('selected')
+    })
+
+    checkOrder();
 
 }
